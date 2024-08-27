9d54a2fb946760a501c8a2f163e525bc49e0f44a
veerrajsingh <86839287+veerrajsingh@users.noreply.github.com>
2024-08-27 03:53:24 +0000

    Added Datatypes.js file

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all JS code as newer version of js
+
+// alert(3+3) // we are using node.js not browser
+
+let name = "Veer"
+let age = 22
+let isLogedIn = false
+
+
+// number => 2 to the power 53
+// bigint
+// string => " "
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+
+// object
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+
