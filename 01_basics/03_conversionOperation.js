718f3eeea05159e67fc169f6eeef01bc77441294
veerrajsingh <86839287+veerrajsingh@users.noreply.github.com>
2024-08-27 04:42:12 +0000

    Conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,22 @@
+let score = "33abc"
+
+console.log(typeof score)
+console.log(typeof (score))
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+// null => 0
+
+let isLogedIn = 1
+console.log(Boolean(isLogedIn));
+
+
+// 1 => true; 0 => false
+// "" => false
+//"Veer" => true
