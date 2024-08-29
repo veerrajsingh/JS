13c6d6d46732c52f81c3378c35f72b6c12abd4d1
veerrajsingh <86839287+veerrajsingh@users.noreply.github.com>
2024-08-29 17:16:35 +0000

    learned type conversion

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,11 +1,11 @@
 let score = "33abc"
 
-console.log(typeof score)
-console.log(typeof (score))
+// console.log(typeof score)
+// console.log(typeof (score))
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber)
-console.log(valueInNumber)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
 
 
 // "33" => 33
@@ -14,9 +14,48 @@ console.log(valueInNumber)
 // null => 0
 
 let isLogedIn = 1
-console.log(Boolean(isLogedIn));
+// console.log(Boolean(isLogedIn));
 
 
 // 1 => true; 0 => false
 // "" => false
 //"Veer" => true
+
+
+//*******************************Operations*******************************
+
+let value = 3
+let negValue = -value
+
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "Hello"
+let str2 = " Veer"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2")
+// console.log( "1" + 2 + 2);
+// console.log( 1 + 2 + "2");
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++
+// console.log(gameCounter);
+
+
